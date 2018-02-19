373edc26064368f9efa7f760880041b012ca6919
Denis Kurbatov <kurbatov.work@gmail.com>
2018-02-19 22:15:36 +0300

    Исправлена 7 задача

--- exer7/Exchange.rb
+++ exer7/Exchange.rb
@@ -1,11 +1,18 @@
 class Numeric
     Rate = {euro:43.61, dollar:32.26, ruble:1}
-    def method_missing (met)    
-        currency = met.to_s.delete('s').to_sym
-        if Rate.key?(currency)
-            self * Rate[currency]
-        end
+    
+    def euro 
+        self*Rate[:euro]
     end
+    def dollar
+        self*Rate[:dollar]
+    end
+    def ruble
+        self
+    end
+    alias :euros :euro
+    alias :dollars :dollar 
+    alias :rubles :ruble 
 
     def in(currency_sym)
         currency_sym = currency_sym.to_s.delete('s').to_sym
