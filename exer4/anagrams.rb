c754da7d87aba15e74056ae7582d9ce7361d3f69
Denis Kurbatov <kurbatov.work@gmail.com>
2018-02-19 20:31:25 +0300

    Исправлена 4 задача

--- exer4/anagrams.rb
+++ exer4/anagrams.rb
@@ -1,28 +1,6 @@
-words = ["cars", "for", "potatoes", "racs", "four", "scar", "creams", "scream"]
 
 def combine_anagrams(words)
-  combine = [[]]
-
-  words.each { |e|
-    flag = false
-      if combine[0].empty?
-        combine[0][0] = e
-        next
-      else
-        combine.each { |arr|
-          if arr[0].split(//).sort == e.split(//).sort
-            arr << e
-            flag = true
-            break;
-          end
-
-          }
-          next if flag
-          combine << [e]
-      end
-
-    }
-    combine
+  hash =  words.group_by {|word| word.chars.sort}
+  array = hash.values
 end
 
-print combine_anagrams(words)
